c06026d83fdfd0d7556ec64aca17724de85a9603
William Nogueira <will.ribeiro@live.com>
2021-10-15 18:15:33 -0300

    validando visitante

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useState, useContext } from "react"
 import { AuthContext } from "../Contexts/AuthContext";
 import {parseCookies} from "nookies"
 import { GetServerSideProps } from "next";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -30,19 +31,8 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx); 
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   };
-}
+})
