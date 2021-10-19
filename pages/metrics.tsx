a6353984214c2fcc199af5cad5efafd05f447fdb
William Nogueira <will.ribeiro@live.com>
2021-10-18 23:25:14 -0300

    validando permissões (server)

--- pages/metrics.tsx
+++ pages/metrics.tsx
@@ -1,23 +1,12 @@
-import { useContext, useEffect } from "react";
 import { Can } from "../components/Can";
-import { AuthContext } from "../contexts/AuthContext";
 import { setupAPIClient } from "../services/api";
-import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
-export default function Dashboard() {
-  const {user, isAuthenticated} = useContext(AuthContext)
-
+export default function Metrics() {
   
-
-  useEffect(() => {
-    api.get('/me')
-      .then(response =>  console.log(response)) 
-  }, [])
-
   return (
     <>
-    <h1>Dashboard: {user?.email}</h1>
+    <h1>Metrics</h1>
 
     <Can permissions={['metrics.list']}>
       <div>Métricas</div>
@@ -29,10 +18,11 @@ export default function Dashboard() {
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
   const response = await apiClient.get('/me');
-  
-  console.log(response.data)
 
   return {
     props: {}
   }
+}, {
+  permissions: ['metrics.list'],
+  roles: ['administrator']
 }) 
